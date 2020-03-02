ece638997944c7dc1d8b1c1a9debea8d5d8cce51
sameyasu <sameyasu@gmail.com>
2020-03-02 22:10:21 +0900

    CIでのビルド対象バージョンに7.4を追加

--- ci/build.sh
+++ ci/build.sh
@@ -5,7 +5,7 @@ set -e
 RELEASE_TAG=$CI_COMMIT_REF_NAME
 DOCKER_TAG=$CI_COMMIT_SHA
 
-php_envs="7.2-apache 7.2-batch 7.2-phpcs 7.2-cli 7.3-apache 7.3-batch 7.3-phpcs 7.3-cli"
+php_envs="7.2-apache 7.2-batch 7.2-phpcs 7.2-cli 7.3-apache 7.3-batch 7.3-phpcs 7.3-cli 7.4-apache 7.4-batch 7.4-phpcs 7.4-cli"
 
 for php_env in $php_envs
 do
